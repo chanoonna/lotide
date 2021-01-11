daa85d18e749944c78fe366ee4719ce47a08d3e5
chanoonna <chanoonna@gmail.com>
2021-01-11 21:33:01 +0000

    added throw error for head and tail

--- head.js
+++ head.js
@@ -1,3 +1,8 @@
-const head = (arr) => arr.length > 0 ? arr[0] : undefined;
+const head = function(arr) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError("The argument is not an array.");
+  }
+  return arr.length > 0 ? arr[0] : undefined;
+};
 
 module.exports = head;
--- tail.js
+++ tail.js
@@ -1,3 +1,8 @@
-const tail = (arr) => arr.length > 1 ? arr.slice(1) : [];
+const tail = function(arr) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError("The argument is not an array.");
+  }
+  return arr.length > 1 ? arr.slice(1) : [];
+};
 
 module.exports = tail;
