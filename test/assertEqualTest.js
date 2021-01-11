e85f6b2879de583b9bb133e8fece7a4f822b5e39
chanoonna <chanoonna@gmail.com>
2021-01-11 20:04:22 +0000

    removed test files

--- test/assertEqualTest.js
+++
@@ -1,6 +0,0 @@
-const assertEqual = require('../assertEqual');
-
-assertEqual(1, 1);
-assertEqual(12, 12);
-assertEqual(true, true);
-assertEqual(undefined, undefined);
--- test/headTest.js
+++
@@ -1,6 +0,0 @@
-const head = require('../head.js');
-const assertEqual = require('../assertEqual.js');
-
-assertEqual(head([1, 2, 3, 4]), 1);
-assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
