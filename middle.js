532ce20ed526d3bdf86b566bed9092b75110df4e
chanoonna <chanoonna@gmail.com>
2021-01-11 21:41:35 +0000

    middle.js : added throw Error

--- middle.js
+++ middle.js
@@ -1,10 +1,13 @@
-const middle = (array) => {
-  if (array.length < 3) {
+const middle = function(arr) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError("The argument is not an array.");
+  }
+  if (arr.length < 3) {
     return [];
   }
-  const middle = Math.floor(array.length / 2);
+  const middle = Math.floor(arr.length / 2);
 
-  return array.length & 1 ? [ array[middle] ] : [ array[middle - 1], array[middle] ]; // & 1 returns 1 for odd, 0 for even.
+  return arr.length & 1 ? [ arr[middle] ] : [ arr[middle - 1], arr[middle] ]; // & 1 returns 1 for odd, 0 for even.
 };
 
 module.exports = middle;
